113f6ccce63e46bc5e71078a382ff6d43f88cbfc
kimyu0218 <serena35@ewhain.net>
2024-08-30 00:50:52 +0900

    feat: ExceptionInterceptor 전역으로 사용 [#145]
    APP_INTERCEPTOR를 통해 애플리케이션 전체에서 전역으로 사용할 수 있도록
    구현

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { APP_INTERCEPTOR } from '@nestjs/core';
 import { AppController } from './app.controller';
 import { ClovaModule } from './clova/clova.module';
 import { RedisModule } from './common/cache/redis/redis.module';
+import { ExceptionInterceptor } from './common/exception/exception.interceptor';
 import { LoggerModule } from './common/log/logger.module';
 import { SpellModule } from './spell/spell.module';
 
@@ -15,5 +17,11 @@ import { SpellModule } from './spell/spell.module';
     LoggerModule,
   ],
   controllers: [AppController],
+  providers: [
+    {
+      provide: APP_INTERCEPTOR,
+      useClass: ExceptionInterceptor,
+    },
+  ],
 })
 export class AppModule {}
