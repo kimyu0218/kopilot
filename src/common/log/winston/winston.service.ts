6f4cb4b27e663ebab5982bb4a308e6c965d7c4c6
kimyu0218 <serena35@ewhain.net>
2024-08-30 00:25:41 +0900

    rename: 변수명 변경 [#145]
    prefix -> context로 직관적인 변수명으로 대체

--- src/common/log/winston/winston.service.ts
+++ src/common/log/winston/winston.service.ts
@@ -2,30 +2,27 @@ import { Logger } from 'winston';
 import { Injectable } from '@nestjs/common';
 import { winstonLogger } from './winston.logger';
 
-const COMMON_PREFIX: string = 'LOG ';
-
 @Injectable()
 export class WinstonService {
   private readonly logger: Logger = winstonLogger();
 
-  debug(message: string, prefix: string = 'Unknown', ...args: any): void {
-    this.logger.debug(this.makeLogMessage(message, prefix), ...args);
+  debug(message: string, context: string = 'Unknown', ...args: any): void {
+    this.logger.debug(this.makeLogMessage(message, context), ...args);
   }
 
-  info(message: string, prefix: string = 'Unknown', ...args: any): void {
-    this.logger.info(this.makeLogMessage(message, prefix), ...args);
+  info(message: string, context: string = 'Unknown', ...args: any): void {
+    this.logger.info(this.makeLogMessage(message, context), ...args);
   }
 
-  warn(message: string, prefix: string = 'Unknown', ...args: any): void {
-    this.logger.warn(this.makeLogMessage(message, prefix), ...args);
+  warn(message: string, context: string = 'Unknown', ...args: any): void {
+    this.logger.warn(this.makeLogMessage(message, context), ...args);
   }
 
-  error(message: string, prefix: string = 'Unknown', trace?: string): void {
-    const logMessage: string = this.makeLogMessage(message, prefix);
-    this.logger.error(trace ? `${logMessage}\n${trace}` : logMessage);
+  error(message: string, context: string = 'Unknown', ...args: any): void {
+    this.logger.error(this.makeLogMessage(message, context), ...args);
   }
 
-  private makeLogMessage(message: string, prefix: string): string {
-    return `${COMMON_PREFIX}[${prefix}] ${message}`;
+  private makeLogMessage(message: string, context: string): string {
+    return `[${context}] ${message}`;
   }
 }
